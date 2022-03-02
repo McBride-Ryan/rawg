86923c6095edd5511b10562a8bd1a20440ba6503
mcbride-ryan <ry.mcbride9@gmail.com>
2022-03-02 16:21:10 -0500

    Video Game App Built in ReactJS

---
+++ src/request.js
@@ -0,0 +1,7 @@
+// const APIKEY = "c1967f2e93fd4b4686fdfa369f0949f6"
+
+const requests = {
+    fetchGames: "https://api.rawg.io/api/games?key=c1967f2e93fd4b4686fdfa369f0949f6&ordering",
+}
+
+export default requests;
