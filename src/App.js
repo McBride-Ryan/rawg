86771f4cb857b232ad9c63559db19fb41cd254fa
mcbride-ryan <ry.mcbride9@gmail.com>
2022-03-02 16:40:37 -0500

    adds tailwind to React project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 import Card from "./Components/Card";
 import requests from "./request";
 
 function App() {
   return (
-    <Card title="Trending Now" fetchUrl={requests.fetchGames} />
+    <div className="bg-gray-900">
+      <Navbar/>
+      <Card title="Trending Now" fetchUrl={requests.fetchGames} />
+    </div>
   );
 }
 
